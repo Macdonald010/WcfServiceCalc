efde8402a7bcebbda4acf0480954dc0c48efbc4b
Macdonald <Macdonald@LAPTOP-R5BSPKA9>
2024-12-17 07:39:35 +0000

    Add project files.

---
+++ IService1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceCalc
+{
+    // Define the service contract
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+
+        [OperationContract]
+        double Subtract(double n1, double n2);
+
+        [OperationContract]
+        double Multiply(double n1, double n2);
+
+        [OperationContract]
+        double Divide(double n1, double n2);
+
+        [OperationContract]
+        double Modulus(double n1, double n2);
+
+        [OperationContract]
+        double Logarithm(double n1, double baseValue);
+
+        [OperationContract]
+        double Exponential(double baseValue, double exponent);
+
+        [OperationContract]
+        double SquareRoot(double n);
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceCalc
+{
+    public class Service1 : IService1
+    {
+        public double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            if (n2 == 0)
+                throw new ArgumentException("Division by zero is not allowed.");
+            return n1 / n2;
+        }
+
+        public double Modulus(double n1, double n2)
+        {
+            if (n2 == 0)
+                throw new ArgumentException("Modulus by zero is not allowed.");
+            return n1 % n2;
+        }
+
+        public double Logarithm(double n1, double baseValue)
+        {
+            if (n1 <= 0 || baseValue <= 0 || baseValue == 1)
+                throw new ArgumentException("Invalid input for logarithm.");
+            return Math.Log(n1, baseValue);
+        }
+
+        public double Exponential(double baseValue, double exponent)
+        {
+            return Math.Pow(baseValue, exponent);
+        }
+
+        public double SquareRoot(double n)
+        {
+            if (n < 0)
+                throw new ArgumentException("Square root of a negative number is not allowed.");
+            return Math.Sqrt(n);
+        }
+    }
+}
